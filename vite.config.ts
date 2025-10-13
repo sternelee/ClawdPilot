eecadce77862975760ff04389de7517e97ebb887
sternelee <sternelee@gmail.com>
2025-10-13 18:55:21 +0800

    feat: 支持 ip 使用开发环境

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [solid()],
   clearScreen: false,
   server: {
+    host: true, // 允许从网络访问，包括 localhost 和系统 IP
     port: 1420,
     strictPort: true,
     watch: {
